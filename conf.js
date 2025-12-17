523a797851bfb77008469d02900cafdddc135357
lianyis <i@lianyi.me>
2025-12-17 15:56:58 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,44 +1,17 @@
 jsproxy_config({
-  ver: '125', // 再次升级版本号
+  ver: '130', // 猛加版本号，确保不缓存
   static_assets: [],
   
-  // 【关键修改】使用基于域名的绝对路径
-  // 这样 sw.js 里的 importScripts 就会拼接成：
-  // /jsproxy/assets/bundle.c33e24c5.js
-  assets_cdn: '/jsproxy/assets/',
+  // 必须是这个，末尾要有斜杠
+  assets_cdn: 'assets/',
 
-  index_path: 'index_v4.html',
+  // 你的首页文件名，确保这个文件在根目录或者 assets 目录能访问到
+  // 建议把 index_v4.html 复制一份到根目录叫 index.html
+  index_path: 'index.html', 
   
-  // 其他配置保持默认即可
   node_map: {
-    'mysite': {
-      label: 'Local',
-      lines: {
-        [location.host]: 1,
-      }
-    },
-    'cfworker': {
-      label: '',
-      hidden: true,
-      lines: {}
-    }
+    'mysite': { label: 'Local', lines: { [location.host]: 1 } }
   },
   node_default: 'mysite',
   node_acc: 'cfworker',
-})
-
-
-  direct_host_list: 'cors_v1.txt',
-  inject_html: '<!-- custom html -->',
-  url_handler: {
-    'https://www.baidu.com/img/baidu_resultlogo@2.png': {
-      replace: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'
-    },
-    'https://www.pornhub.com/': {
-      redir: 'https://php.net/'
-    },
-    'http://haha.com/': {
-      content: 'Hello World'
-    },
-  }
 })
